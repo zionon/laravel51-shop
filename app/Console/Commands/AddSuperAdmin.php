ae553fde8fbfcd0be9c7e03e5a36959210bae15b
gh-huang <gh_huang@outlook.com>
2016-08-30 21:34:43 +0800

    time

---
+++ app/Console/Commands/AddSuperAdmin.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Models\Admin;
+
+class AddSuperAdmin extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'make:superadmin';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Add SuperAdministartor';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $admin = new Admin();
+        $admin->name = 'admin';
+        $admin->email = 'admin@admin.com';
+        $admin->password = 'password';
+    }
+}
